2dc989d504bc6f4fcab573ea81be73c57693050a
Sansyzbay Asylkhan <sansuzbaiasik@gmail.com>
2026-01-22 13:32:54 +0500

    Sansyzbay Assylkhan:add services

--- src/ticketing/services/TicketService.java
+++ src/ticketing/services/TicketService.java
@@ -1,20 +1,37 @@
 package ticketing.services;
 
-import ticketing.exceptions.InvalidTicketException;
+import ticketing.entities.Ticket;
 import ticketing.repositories.TicketRepository;
+
+import java.util.List;
 
 public class TicketService {
 
-    private final TicketRepository ticketRepository;
+    private final TicketRepository ticketRepo;
 
-    public TicketService(TicketRepository ticketRepository) {
-        this.ticketRepository = ticketRepository;
+    public TicketService(TicketRepository ticketRepo) {
+        this.ticketRepo = ticketRepo;
     }
 
+    /
     public void validateTicket(String code) {
-        if (!ticketRepository.existsByCode(code)) {
-            throw new InvalidTicketException();
+        ticketRepo.validateTicket(code);
+    }
+
+
+    public void showAllTickets() {
+        List<Ticket> tickets = ticketRepo.getAllTickets();
+        System.out.println("All tickets:");
+        for (Ticket t : tickets) {
+            System.out.println(t);
         }
-        System.out.println("Ticket is valid");
+    }
+
+    public void showUsedTickets() {
+        List<Ticket> tickets = ticketRepo.getUsedTickets();
+        System.out.println("Used tickets:");
+        for (Ticket t : tickets) {
+            System.out.println(t);
+        }
     }
 }
