1a2da66307a11a2469fa5b33df235d42b02196b7
Sansyzbay Asylkhan <sansuzbaiasik@gmail.com>
2026-01-31 15:35:49 +0500

    Sansyzbay Assylkhan:add services

--- src/ticketing/services/TicketService.java
+++ src/ticketing/services/TicketService.java
@@ -6,12 +6,12 @@ import ticketing.utils.DiscountManager;
 
 public class TicketService {
 
-    // Покупка билета
+
     public Ticket buyTicket(String type, int seatNumber, double price, String customerType) {
         // Singleton для управления скидками
         double finalPrice = DiscountManager.getInstance().applyDiscount(price, customerType);
 
-        // Factory для создания нужного типа билета
+
         return TicketFactory.createTicket(type, seatNumber, finalPrice);
     }
 }
