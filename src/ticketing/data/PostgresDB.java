2070cab896a3716602923709e39b0213f44c6b7a
Aikorkem <ajkorkemmuktar@gmail.com>
2026-01-31 18:40:08 +0500

    Muktar Aikorkem:add Main java

--- src/ticketing/data/PostgresDB.java
+++ src/ticketing/data/PostgresDB.java
@@ -2,23 +2,36 @@ package ticketing.data;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
 
 public class PostgresDB implements IDB {
 
-    private static final String URL =
-            "jdbc:postgresql://aws-1-ap-southeast-2.pooler.supabase.com:5432/postgres";
+    private final String url;
+    private final String user;
+    private final String password;
 
-    private static final String USER =
-            "postgres.olpszwzwhedzckiiraxm";
+    public PostgresDB() {
+        try {
+            Properties props = new Properties();
+            var in = getClass().getClassLoader().getResourceAsStream("config.properties");
+            if (in == null) {
+                throw new RuntimeException("config.properties not found in resources");
+            }
+            props.load(in);
 
-    private static final String PASSWORD =
-            "Aikosha.2007";
+            this.url = props.getProperty("db.url");
+            this.user = props.getProperty("db.user");
+            this.password = props.getProperty("db.password");
+
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to load DB config", e);
+        }
+    }
+
     @Override
     public Connection getConnection() {
         try {
-            Connection con = DriverManager.getConnection(URL, USER, PASSWORD);
-            System.out.println("Connected to Supabase ");
-            return con;
+            return DriverManager.getConnection(url, user, password);
         } catch (Exception e) {
             System.out.println("Failed to connect to Supabase");
             throw new RuntimeException(e);
