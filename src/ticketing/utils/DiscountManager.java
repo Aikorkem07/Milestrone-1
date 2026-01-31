e4fcdc76391f0936fa03ea0abccfc0d49ba6f3e3
Aikorkem <ajkorkemmuktar@gmail.com>
2026-01-31 15:41:21 +0500

    Muktar Aikorkem:add utils

---
+++ src/ticketing/utils/DiscountManager.java
@@ -0,0 +1,20 @@
+package ticketing.utils;
+
+public class DiscountManager {
+    private static DiscountManager instance;
+
+    private DiscountManager() {}
+
+    public static DiscountManager getInstance() {
+        if (instance == null) instance = new DiscountManager();
+        return instance;
+    }
+
+    public double applyDiscount(double price, String customerType) {
+        return switch (customerType.toUpperCase()) {
+            case "VIP" -> price * 0.8;
+            case "STUDENT" -> price * 0.9;
+            default -> price;
+        };
+    }
+}
---
+++ src/ticketing/utils/SearchResult.java
@@ -0,0 +1,10 @@
+package ticketing.utils;
+
+import java.util.List;
+
+public class SearchResult<T> {
+    private List<T> results;
+
+    public SearchResult(List<T> results) { this.results = results; }
+    public List<T> getResults() { return results; }
+}
